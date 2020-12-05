35bbc3dd9410cc94e8b39ae316a584cf54390fb2
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-04 19:27:47 -0500

    app check displays on webpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* content goes here */}
+        App
       </div>
     )
   }
