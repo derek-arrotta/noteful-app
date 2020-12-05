33ad1a252fd2999060d4a53a602a48dc37a8a6aa
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-05 18:21:53 -0500

    add state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default class App extends Component {
+  
+  state = {
+    folders: [],
+    notes: [],
+  }
+
   render() {
     return (
       <div className="App">
-        App
+        <h1>Noteful</h1>
+
+
       </div>
     )
   }
