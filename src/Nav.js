df19037249ec5ef6b6319e277b12cde9b03022ab
Derek Arrotta <derek.arrotta@gmail.com>
2020-12-07 08:50:03 -0500

    pasting from solution

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class SideBar extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>Note 1</li>
+          <li>Note 2</li>
+          <li>Note 3</li>
+          <li>Add Note</li>
+        </ul>
+      </nav>
+    )
+  }
+}
